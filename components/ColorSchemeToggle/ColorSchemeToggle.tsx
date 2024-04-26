c850bcccf8026b527c380e029e1a51ae3a3e0df2
weeb js <tahfaric@nycstudents.net>
2024-04-26 02:15:32 +0000

    feat: 🤫

--- components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,13 +1,112 @@
-import { Button, Group, useMantineColorScheme } from '@mantine/core';
+'use client';
+
+import React from 'react';
+import { Button, Group, useMantineColorScheme, Accordion, Container, Title, Code, useMantineTheme, Card, rem, Text, Badge, SimpleGrid } from '@mantine/core';
+import { IconExternalLink, IconGauge, IconUser, IconCookie, IconClock, IconMoodSmileBeam, IconInfoCircle } from '@tabler/icons-react';
+import classes from './FaqSimple.module.css';
+import classes1 from './FeaturesCards.module.css';
+const mockdata = [
+  {
+    title: 'Utility',
+    description:
+      "Zap's utility commands are designed to provide users with quick access to informational content, or info based on your server.",
+    icon: IconInfoCircle,
+  },
+  {
+    title: 'Reminders',
+    description:
+      "Zap's reminder system allows users to create, view, and remove reminders conveniently.",
+    icon: IconClock,
+  },
+  {
+    title: 'Fun',
+    description:
+      "Zap's fun commands are designed to provide users with entertaining and lighthearted experiences.",
+    icon: IconMoodSmileBeam,
+  },
+  {
+    title: 'Anime',
+    description: "Users can use Zap’s anime commands to get information on there favourite anime’s and manga, as well as generate waifu, neko, and maid images.",
+    icon: IconCookie,
+  },
+];
+
+const data = [
+  {
+    title: 'Page views',
+    stats: '456,133',
+    description: '24% more than in the same month last year, 33% more that two years ago',
+  },
+  {
+    title: 'New users',
+    stats: '2,175',
+    description: '13% less compared to last month, new user engagement up by 6%',
+  },
+  {
+    title: 'Completed orders',
+    stats: '1,994',
+    description: '1994 orders were completed this month, 97% satisfaction rate',
+  },
+];
+
 
 export function ColorSchemeToggle() {
   const { setColorScheme } = useMantineColorScheme();
+  const theme = useMantineTheme();
+  const icon = <IconExternalLink size={14} />;
+
+  const features = mockdata.map((feature) => (
+    <Card key={feature.title} shadow="md" radius="md" className={classes1.card} padding="xl">
+      <feature.icon
+        style={{ width: rem(50), height: rem(50) }}
+        stroke={2}
+        color="#FFFF00"
+      />
+      <Text fz="lg" fw={500} className={classes.cardTitle} mt="md">
+        {feature.title}
+      </Text>
+      <Text fz="sm" c="dimmed" mt="sm">
+        {feature.description}
+      </Text>
+    </Card>
+  ));
 
   return (
-    <Group justify="center" mt="xl">
-      <Button onClick={() => setColorScheme('light')}>Light</Button>
-      <Button onClick={() => setColorScheme('dark')}>Dark</Button>
-      <Button onClick={() => setColorScheme('auto')}>Auto</Button>
-    </Group>
+    <>
+      <Group justify="center" mt="xl" >
+        <Button
+          justify="center"
+          component="a"
+          href="https://www.guilded.gg/b/2e702266-2dfe-4796-b61c-ccbb0536444c"
+          size="lg"
+          rightSection={icon}
+          variant="default"
+          mt="md"
+        >
+          Invite
+        </Button>
+        <Button
+          justify="center"
+          component="a"
+          href="https://www.guilded.gg/i/knxezNqE"
+          size="lg"
+          rightSection={icon}
+          variant="default"
+          mt="md"
+        >
+          Support Server
+        </Button>
+      </Group>
+      <Container size="lg" mt={200} py="xl">
+        <Title order={2} className={classes1.title} ta="center" mt="sm">
+          Features 👇
+        </Title>
+        <SimpleGrid cols={{ base: 2, md: 2 }} spacing="xl" mt={50}>
+          {features}
+        </SimpleGrid>
+      </Container>
+      <Container size="sm" mt={210} className={classes.wrapper}>
+        </Container>
+    </>
   );
 }
--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,23 +1,109 @@
-import { Title, Text, Anchor } from '@mantine/core';
+"use client";
+import {
+  Title,
+  Text,
+  Anchor,
+  AppShell,
+  Burger,
+  Flex,
+  Image,
+  Card,
+  SimpleGrid,
+  Container,
+  rem,
+  useMantineTheme,
+  Group,
+  Badge,
+  Button,
+  UnstyledButton,
+  MantineProvider,
+  useComputedColorScheme,
+  useMantineColorScheme,
+  Affix,
+  Transition,
+  BackgroundImage,
+  Switch,
+  AppShellHeader,
+  ActionIcon,
+  Avatar,
+  Modal,
+  Code
+} from '@mantine/core';
+
+import { Spotlight } from '@mantine/spotlight';
+import { useDisclosure } from '@mantine/hooks';
+import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons-react';
 import classes from './Welcome.module.css';
+import { useWindowScroll } from '@mantine/hooks';
+import { IconArrowUp, IconHome, IconDashboard, IconFileText, IconSearch, IconSun, IconMoonStars } from '@tabler/icons-react';
+import { IconGauge, IconUser, IconCookie } from '@tabler/icons-react';
+import classes1 from './MobileNavbar.module.css';
+import '@mantine/spotlight/styles.css';
+import { FaSun, FaMoon, FaGuilded, FaLink } from 'react-icons/fa';
+import { IoMdPulse } from 'react-icons/io'
+import { FaNewspaper, FaXTwitter } from "react-icons/fa6";
+import { IoSunny } from "react-icons/io5";
+import { FaInfoCircle } from 'react-icons/fa';
+const icon = <IconSearch size={14} />
 
 export function Welcome() {
+  // Hook to control the color 
+  const [opened, { open, close }] = useDisclosure(false);
+  const theme = useMantineTheme();
+  const [scroll, scrollTo] = useWindowScroll();
+  const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme('light');
+  const toggleColorScheme = () => {
+    setColorScheme(computedColorScheme === "dark" ? 'light' : 'dark');
+  }
+
   return (
-    <>
-      <Title className={classes.title} ta="center" mt={100}>
-        Welcome to{' '}
-        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+    <MantineProvider defaultColorScheme="dark">
+      <AppShell header={{ height: 70 }} padding="md">
+        <AppShell.Header>
+          <Group justify="space-between" style={{ flex: 1 }}>
+            <IoMdPulse
+              color='#EAD5FF'
+            size={65}
+              radius="md"
+              h={70}
+              w={70}
+              />
+
+            <Group ml="xl" gap={9} visibleFrom="sm" mr={40}>
+            </Group>
+          </Group>
+        </AppShell.Header>
+      </AppShell>
+      <Title className={classes.title} ta="center" mt={160}>
+        <Text
+          inherit
+          variant="gradient"
+          component="span"
+          gradient={{ from: '#EAD5FF', to: '#EAD5FF' }}
+        >
+          Pulse
         </Text>
       </Title>
-      <Text color="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Next.js project includes a minimal setup for server side rendering, if you want
-        to learn more on Mantine + Next.js integration follow{' '}
-        <Anchor href="https://mantine.dev/guides/next/" size="lg">
-          this guide
-        </Anchor>
-        . To get started edit index.tsx file.
-      </Text>
-    </>
+      <Group justify='center'>
+        <Text className={classes.textLeft} c="dimmed" ta="center" mr={60} size="lg" maw={580} mt="xl">
+          Pulse is a moderation & utility bot designed to view things more “moderately”. It help’s with taking care of things without a breeze!{' '}
+        </Text>
+      </Group>
+      <Affix position={{ bottom: 20, right: 20 }}>
+        <Transition transition="slide-up" mounted={scroll.y > 0}>
+          {(transitionStyles) => (
+            <Button
+              leftSection={<IconArrowUp style={{ width: rem(16), height: rem(16) }} />}
+              style={transitionStyles}
+              onClick={() => scrollTo({ y: 0 })}
+              variant='default'
+            >
+              Scroll to top
+            </Button>
+          )}
+        </Transition>
+      </Affix>
+    </MantineProvider>
   );
 }
